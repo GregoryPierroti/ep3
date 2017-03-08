b4df9fd7015da8d72ed8f2c05261b3902cfcf054
GregoryPierroti <gregorypierroti@hotmail.com>
2017-03-08 11:16:14 -0300

    Principal.java
    Principal.java

---
+++ Principal.java
@@ -0,0 +1,22 @@
+public class Principal{
+	public static void main(String[] args){		
+		ArrayList<Aluno> aluno = new ArrayList();
+		aluno a1=new Aluno();
+		aluno a2=new Aluno();
+		aluno a3=new Aluno();
+		aluno a4=new Aluno();
+		aluno a5= new Aluno();
+		
+		a1.setNome("Felipe");
+		a1.setIdade(28);
+		a2.setNome("Victor");
+		a2.setIdade(12);
+		a3.setNome("Caio");
+		a3.setIdade(16);
+		a4.setNome("Grégory");
+		a4.setIdade(17);
+		a5.setNome("João Paulo")
+		a5.setIdade(18);
+		
+	}
+}
