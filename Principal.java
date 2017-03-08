f4fee2052254b79d510bd1897b3c2010f1ff4838
GregoryPierroti <gregorypierroti@hotmail.com>
2017-03-08 11:14:37 -0300

    Principal.java
    Principal.java

---
+++ Principal.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+public class Aluno.java{
+	private String nome;
+	private int idade;
+	public void setNome(){
+		this.nome=nome;
+	}
+	public void setIdade(){
+		this.idade=idade;		
+	}
+	public String getNome(){
+		return this.nome;	
+	}
+	public int getIdade(){
+		return this.idade;	
+	}
+	public  ordenar(int x){
+		int i;		
+		for(i=0;i<5;i++){
+			
+		}	
+	}
+}
