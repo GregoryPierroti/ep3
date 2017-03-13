996e784b8b80efac750c169286c291f770c670d1
GregoryPierroti <gregorypierroti@hotmail.com>
2017-03-13 11:53:56 -0300

    aluno pronto
    aluno

--- Aluno.java
+++ Aluno.java
@@ -1,26 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-public class Aluno.java{
-	private String nome;
-	private int idade;
-	public void setNome(){
-		this.nome=nome;
+public class Aluno implements Comparable<Aluno>{
+	String nome;
+	int idade;
+	
+	public Aluno(String nome, int idade){
+		this.nome = nome;
+		this.idade = idade;
 	}
-	public void setIdade(){
-		this.idade=idade;		
-	}
+	
 	public String getNome(){
-		return this.nome;	
+		return this.nome;
 	}
-	public int getIdade(){
-		return this.idade;	
-	}
-	public  ordenar(int x){
-		int i;		
-		for(i=0;i<5;i++){
-			
-		}	
+        public int getIdade(){
+                return this.idade;
+        }
+	public int compareTo(Aluno outro){
+		if(this.idade < outro.getIdade())
+			return 1;
+		else if(this.idade > outro.getIdade())
+			return -1;
+		else
+			return 0;
 	}
 }
