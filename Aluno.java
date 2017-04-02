0c999b54a5c8f2b052f37edd221e6c0c47fbbc50
GregoryPierroti <gregorypierroti@hotmail.com>
2017-04-02 19:37:43 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,4 +1,4 @@
-public class Aluno implements Comparable<Aluno>{
+public class Aluno{
 	String nome;
 	int idade;
 	
@@ -13,12 +13,4 @@ public class Aluno implements Comparable<Aluno>{
         public int getIdade(){
                 return this.idade;
         }
-	public int compareTo(Aluno outro){
-		if(this.idade < outro.getIdade())
-			return 1;
-		else if(this.idade > outro.getIdade())
-			return -1;
-		else
-			return 0;
-	}
 }
